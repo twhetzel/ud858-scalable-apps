1de7a0b1f480302bd83e7e8671b535154b924842
Trish Whetzel <plwhetzel@gmail.com>
2015-11-03 18:20:14 -0800

    add project IDs

--- ConferenceCentral_FINAL/static/js/app.js
+++ ConferenceCentral_FINAL/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '218263164487-3eo97mbj1grcu08sp60qq075em9uin64.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
